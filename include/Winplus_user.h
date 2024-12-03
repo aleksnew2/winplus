1caaaf21641589e7daf4539dca9fa4783bcdb246
aleksnew2 <aleks.koyf@gmail.com>
2024-12-03 20:46:39 +0300

    Winplus_user.h: add WinSizePlus and WinPosPlus

--- include/Winplus_user.h
+++ include/Winplus_user.h
@@ -1,2 +1,14 @@
 #include "Winplus_defines.h"
 #include "Winplus_types.h"
+
+extern "C" {
+typedef struct {
+  u16 Width;
+  u16 Height;
+} WinSizePlus;
+
+typedef struct {
+  i16 PosX;
+  i16 PosY;
+} WinPosPlus;
+}
