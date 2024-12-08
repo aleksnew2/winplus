e21779f30c078618719ea540a18801b5f91f5046
aleksnew2 <aleks.koyf@gmail.com>
2024-12-08 16:50:44 +0300

    Winplus_types.hpp: add error_code and id_code

--- include/Winplus_error.hpp
+++ include/Winplus_error.hpp
@@ -1,1 +1,19 @@
 #include "Winplus_defines.hpp"
+
+namespace winplus::error {
+/**
+ * Terminates the current process with an error code.
+ *
+ * This function is used internally for error handling and should not be called
+ * directly. Useful for halting execution when an unrecoverable error occurs.
+ * Outputs error before stopping program process. 
+ */
+WINPLUS_API void StopProcess(u16 error);
+
+/**
+  Outputs error with code.
+
+
+ */
+WINPLUS_API void Log();
+} // namespace winplus::error
--- test.c++
+++ test.c++
@@ -1,8 +1,7 @@
 #include "include/Winplus.hpp"
 #include <print>
 
-int main() { 
-  auto window = winplus::user::WP_Init(50, 60, 500, 500, "Window"); 
-  std::println("{}", window.Id);
-  window.Open();
+int main() {
+  const auto code = winplus::rand::GenerateErrorCode();
+  std::println("{}", code);
 }
